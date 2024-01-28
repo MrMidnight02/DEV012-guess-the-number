8f59399571f5e65d9054e9bf352e4100d022729f
MrMidnight02 <itzel.garce@gmail.com>
2024-01-28 11:22:54 -0600

    El juego es para la computadoray un usario

--- guessNumber.java
+++ guessNumber.java
@@ -12,7 +12,8 @@ public class guessNumber {
         System.out.println("Adivina el número");
         System.out.println("Elige un número del 0 al 100");
 
-        for (intentos = 1; ; intentos++) {
+        for (intentos = 1; ; intentos ++) {
+            // Turno del jugador
             Scanner entrada = new Scanner(System.in);
             System.out.println("Tu turno: ");
             numeroIngresado = entrada.nextInt();
@@ -23,9 +24,21 @@ public class guessNumber {
                 System.out.println("El número mágico es mayor a " + numeroIngresado);
             } else {
                 System.out.println("¡Felicidades, adivinaste el número mágico en " + intentos + " intentos!");
-                break; // Salir del bucle cuando se adivina el número correcto
+                return; // Terminar el programa cuando se adivina el número correcto
+            }
+
+            // Turno de la computadora
+            int intentoComputadora = nRandom.nextInt(100);
+            System.out.println("Turno de la computadora: La computadora elige " + intentoComputadora);
+
+            if (numeroRandom < intentoComputadora) {
+                System.out.println("El número mágico es menor a " + intentoComputadora);
+            } else if (numeroRandom > intentoComputadora) {
+                System.out.println("El número mágico es mayor a " + intentoComputadora);
+            } else {
+                System.out.println("¡La computadora adivinó el número mágico en " + (intentos + 1) + " intentos!");
+                return; // Terminar el programa cuando la computadora adivina el número correcto
             }
         }
-
     }
 }
